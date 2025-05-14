6a5a4b19015c97d729c09665b41537653245542b
Michael Tabor <mjtabor23@gmail.com>
2025-05-13 18:27:50 -0600

    setup python api, added hello route

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
 
+from flask import Flask
+from app.routes.hello import hello_bp
+
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(hello_bp)
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
--- app/routes/hello.py
+++ app/routes/hello.py
@@ -1,1 +1,8 @@
 
+from flask import Blueprint, jsonify
+
+hello_bp = Blueprint('hello', __name__)
+
+@hello_bp.route('/hello')
+def hello():
+    return jsonify(message="Hello from the Flask route!")
