e8b72c8c9d8eb5ed4ada5a719ba6e2cdec6968ca
Michael Tabor <mjtabor23@gmail.com>
2025-05-13 18:35:02 -0600

    update route

--- app/routes/hello.py
+++ app/routes/hello.py
@@ -3,6 +3,6 @@ from flask import Blueprint, jsonify
 
 hello_bp = Blueprint('hello', __name__)
 
-@hello_bp.route('/hello')
-def hello():
-    return jsonify(message="Hello from the Flask route!")
+@hello_bp.route('/main', methods=['GET'])
+def main():
+    return jsonify(message="This is our main route")
